835a4a71df79474f52f4f815f148970c3a451880
jackcaron <jacquelin.caron@gmail.com>
2025-06-13 15:46:22 -0400

    use the entries array created in the common internal code

--- crates/target_cpp/src/cpp_snippets/enum_from_json.cpp
+++ crates/target_cpp/src/cpp_snippets/enum_from_json.cpp
@@ -2,11 +2,10 @@
   template<>
   struct Json<$FULL_NAME$> {
     using Enum = $FULL_NAME$;
-    $ENTRIES$
 
     template<typename JValue>
     static ExpType<Enum> deserialize(const JValue &value) {
-      return get_enum_index(value, entries, "$ENUM_NAME$"sv)
+      return get_enum_index(value, Common<Enum>::entries, "$ENUM_NAME$"sv)
         .transform([](auto idx) {
           switch(idx) {
             default:
--- crates/target_cpp/src/cpp_snippets/enum_to_json.cpp
+++ crates/target_cpp/src/cpp_snippets/enum_to_json.cpp
@@ -1,10 +1,5 @@
 
   ExpType<void> serialize(Writer::Serializer& serializer, const $FULL_NAME$ value) {
     using Enum = $FULL_NAME$;
-    const auto strvalue = ([value](){
-          switch (value) {
-            default:
-$CLAUSES$          }
-        })();
-    return serializer.write_str(strvalue);
+    return serializer.write_str(Common<Enum>::entries[size_t(value)]);
   }
